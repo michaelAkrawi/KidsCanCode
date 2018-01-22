a3ce17f7237b4ed282e03efad96be15e2d091811
michaelAkrawi <michael.akrawi@yahoo.com>
2018-01-22 22:20:25 +0200

    bitrix

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-﻿
+﻿$(document).ready(function(){
+
+ $("#btnSend").click(function(){
+
+    var url = `https://kidscancode.bitrix24.com/rest/1/64ubmxtgiaie3lu3/crm.lead.add`;
+    var request =   $.ajax({
+
+      method : 'POST',
+      url : url,
+      data : JSON.stringify( {fields : { title : 'test', name: 'michael', last_name : 'akrawi'}})
+    });
+
+    request.done(function(){ console.log('add'); });
+
+    request.fail(function( jqXHR, textStatus ) {
+      console.log(jqXHR);
+      alert( "Request failed: " + textStatus );
+    });
+
+ });
+
+});
